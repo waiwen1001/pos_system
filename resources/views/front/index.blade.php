b8b4e7ff103cd87d278d4b4990072f5d3db4e56e
ecommerce <wongwaiwen1001@gmail.com>
2021-12-25 15:41:39 +0800

    disable click modal outside to close

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -753,7 +753,7 @@
     </div>
   </div>
 
-  <div class="modal fade" id="clearItemsModal" tabindex="-1" role="dialog" aria-labelledby="clearItemsModalLabel" aria-hidden="true">
+  <div class="modal fade" id="clearItemsModal" tabindex="-1" role="dialog" aria-labelledby="clearItemsModalLabel" aria-hidden="true" data-backdrop="static">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
@@ -903,7 +903,7 @@
     </div>
   </div>
 
-  <div class="modal fade" id="voucherModal" tabindex="-1" role="dialog" aria-labelledby="voucherModalLabel" aria-hidden="true">
+  <div class="modal fade" id="voucherModal" tabindex="-1" role="dialog" aria-labelledby="voucherModalLabel" aria-hidden="true" data-backdrop="static">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
